06c3de970a1e107527b37e69f8342d2bc5222535
kbuchinskiy <kirillsmailbox@gmail.com>
2019-02-05 23:25:47 +0200

    feature: clear cart

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,13 @@
 var cartPopup = document.querySelector("#cartPopup"),
   openCartPopupBtn = document.querySelector("#openCartPopupBtn"),
   cartList = cartPopup.querySelector(".cart-list"),
-  productList = document.querySelector(".product-list");
+  productList = document.querySelector(".product-list"),
+  clearCartBtn = document.querySelector("button[data-action='clear-cart']");
 
 openCartPopupBtn.addEventListener("click", toggleCartPopup);
 productList.addEventListener("click", handleProductClick);
-cartPopup.addEventListener("click", handleProductClick);
+cartList.addEventListener("click", handleProductClick);
+clearCartBtn.addEventListener("click", clearCart);
 
 function toggleCartPopup(e) {
   cartPopup.classList.toggle("opened");
@@ -20,7 +22,6 @@ function handleProductClick(e) {
   if (isButton) {
     action = e.target.getAttribute("data-action");
     productId = e.target.closest("[data-product-id]").dataset.productId;
-
     productToUpdate = getProductFromData(productId);
 
     switch (action) {
@@ -33,8 +34,6 @@ function handleProductClick(e) {
       case "delete":
         removeCartItem(productId, productToUpdate, true);
     }
-    
-    updateTotalPrice();
   }
 }
 
@@ -91,4 +90,58 @@ function removeCartItem(productId, productToUpdate, deleteProduct) {
     updateProductElem(productId);
     updateCartProductElem(productId);
   }
+}
+
+function addNewCartProductElem(productData) {
+  var cartItemElem = document
+    .querySelector("#cartItemTemplate")
+    .content.cloneNode(true);
+  cartItemElem.querySelector(".cart-item").dataset.productId = productData.id;
+  cartItemElem.querySelector(".cart-item-title").innerText = productData.title;
+  cartItemElem.querySelector(".cart-item-amount").innerText =
+    productData.amount;
+  cartItemElem.querySelector(".cart-item-total-price").innerText =
+    productData.totalPrice + " $";
+
+  cartList.appendChild(cartItemElem);
+}
+
+function getCartProductElem(productId) {
+  var elemToUpdate = cartList.querySelector(
+    ".cart-item[data-product-id=" + productId + "]"
+  );
+  return elemToUpdate;
+}
+
+function deleteNewCartProductElem(elemToDelete) {
+  cartList.removeChild(elemToDelete);
+}
+
+function updateCartProductElem(productId) {
+  var updatedItem;
+  elemToUpdate = getCartProductElem(productId);
+
+  if (cart.hasProduct(productId)) {
+    updatedItem = cart.getProductById(productId);
+    elemToUpdate.querySelector(".cart-item-amount").innerText =
+      updatedItem.amount;
+    elemToUpdate.querySelector(".cart-item-total-price").innerText =
+      updatedItem.totalPrice + " $";
+  } else {
+    deleteNewCartProductElem(elemToUpdate);
+  }
+
+  updateTotalPrice();
+}
+
+function clearCart(e) {
+  if (cart.isEmpty()) {
+    return false;
+  }
+  var cartProducts = [...cart.products];
+  cartProducts.forEach(product => {
+    cart.deleteProduct(product.id);
+    updateProductElem(product.id);
+    updateCartProductElem(product.id);
+  });
 }
